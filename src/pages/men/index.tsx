e6fa816be9f29a27ddb888a07fe08f8248faa2f7
KevinZheng99 <kevinzhengkz99@gmail.com>
2024-02-08 16:49:39 -0500

    mens home page image

--- src/pages/men/index.tsx
+++ src/pages/men/index.tsx
@@ -1,10 +1,28 @@
 import React from "react";
+import Box from "@mui/material/Box";
 import NavigationBar from "../../components/NavigationBar";
 
 const Men = () => {
   return (
     <>
       <NavigationBar />
+      <Box
+        sx={{
+          height: "100vh",
+          overflowY: "scroll",
+          scrollSnapType: "y mandatory",
+          scrollBehavior: "smooth",
+        }}
+      >
+        <Box
+          sx={{
+            height: "100vh",
+            backgroundImage: "url(MenKithJacket.jpg)",
+            backgroundRepeat: "no-repeat",
+            scrollSnapAlign: "start",
+          }}
+        />
+      </Box>
     </>
   );
 };
